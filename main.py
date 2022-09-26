54fa2040816796a059814d9433bbeaa549ce7694
tiger <thisisstreamliner@gmail.com>
2022-09-26 22:41:30 +0300

    finish app.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # This is a sample Python script.
 
 # Create a greeting for your program.
-
+print("Welcome to the Band Name Generator.")
 # Ask the user for the city that they grew up in.
-
+city = input("What's name of the city you grew up in?\n")
 # Ask the user for the name of a pet.
-
+pet_name = input("What's your pet's name?\n")
 # Combine the name of their city and pet and show them their band name.
-
+print("Your band name could be " + city + " " + pet_name)
 # Make sure the input cursor shows on a new line
 
